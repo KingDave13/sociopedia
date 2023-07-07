af82cfe9f65dbdee7897c14b29d348fab9646459
David Okaliwe <okaliwedavid@gmail.com>
2023-07-08 00:18:39 +0100

    Fourth commit

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 
 /* CONFIGURATIONS */
@@ -47,6 +48,8 @@ app.post("/auth/register", upload.single("picture"), register);
 /* ROUTES*/
 
 app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
+
 
 
 /* MONGOOSE SETUP */
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -8,6 +8,14 @@ export const verifyToken = async (req, res, next) => {
 			return res.status(403).send('Access Denied');
 		}
 
+		if (token.startsWith('Bearer')) {
+			token = token.slice(7, token.length).trimLeft();
+		}
+
+		const verified = jwt.verify(token, process.env.JWT_SECRET);
+		req.user = verified;
+		next();
+
 	} catch (err) {
 		res.status(500).json({ error: err.message });
 	}
