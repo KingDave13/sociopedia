a9e2bc028c6a33f667a13f0c17e632b6380c2179
David Okaliwe <okaliwedavid@gmail.com>
2023-07-07 12:34:33 +0100

    Second commit

--- server/index.js
+++ server/index.js
@@ -36,3 +36,13 @@ const storage = multer.diskStorage({
 });
 
 const upload = multer({ storage });
+
+/* MONGOOSE SETUP */
+
+const PORT = process.env.PORT || 6001;
+mongoose.connect(process.env.MONGO_URL, {
+	useNewUrlParse: true,
+	useUnifiedTopology: true,
+}).then (() => {
+	app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+}).catch((error) => console.log(`${error} did not connect`))
