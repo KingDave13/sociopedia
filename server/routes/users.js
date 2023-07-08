7d0281e0749c5c7fd6fd860251e52b0d9923b17c
David Okaliwe <okaliwedavid@gmail.com>
2023-07-08 01:07:46 +0100

    Update

--- server/routes/users.js
+++ server/routes/users.js
@@ -7,3 +7,10 @@ const router = express.Router();
 /*READ*/
 
 router.get('/:id', verifyToken, getUser);
+router.get('/:id/friends', verifyToken, getUserFriends);
+
+/*UPDATE*/
+
+router.patch('/:id/:friendId', verifyToken, addRemoveFriend);
+
+export default router;
