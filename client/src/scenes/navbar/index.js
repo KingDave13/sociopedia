37376201a3bd2431e5ae33da3194d19052eff7f6
David Okaliwe <okaliwedavid@gmail.com>
2023-07-10 23:41:22 +0100

    Update

--- client/src/scenes/navbar/index.js
+++ client/src/scenes/navbar/index.js
@@ -26,7 +26,13 @@ const Navbar = () => {
 	const isNonMobileScreens = useMediaQuery('(min-width: 1000px');
 
 	const theme = useTheme();
+	const neutralLight = theme.palette.neutral.light;
+	const dark = theme.palette.neutral.dark;
+	const background = theme.palette.background.default;
+	const primaryLight = theme.palette.primary.light;
+	const alt = theme.palette.background.alt;
 
+	const fullName = `${user.firstName} ${user.lastName}`;
 
 	return (
 		<div>
