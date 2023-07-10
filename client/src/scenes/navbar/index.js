8d82afa350668bc0208f8c4c41d81e5dec703cd7
David Okaliwe <okaliwedavid@gmail.com>
2023-07-11 00:34:25 +0100

    Update

--- client/src/scenes/navbar/index.js
+++ client/src/scenes/navbar/index.js
@@ -37,7 +37,9 @@ const Navbar = () => {
 	return (
 		<FlexBetween padding ='1rem 6%' backgroundColor={alt}>
 			<FlexBetween gap='1.75rem'>
-
+				<Typography>
+					Sociopedia
+				</Typography>
 			</FlexBetween>
 		</FlexBetween>
 	)
