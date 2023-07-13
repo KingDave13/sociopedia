e16c2a52b487e7d71bea2f37a4fb47a8140c4ec2
David Okaliwe <okaliwedavid@gmail.com>
2023-07-13 15:24:14 +0100

    Update

--- client/src/scenes/loginPage/Form.js
+++ client/src/scenes/loginPage/Form.js
@@ -96,6 +96,46 @@ const Form = () => {
 								Boolean(errors.firstName)}
 								helperText={touched.firstName && 
 								errors.firstName}
+								sx={{ gridColumn: 'span 2' }}
+								/>
+
+								<TextField 
+								label='Last Name'
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.lastName}
+								name='lastName'
+								error={Boolean(touched.lastName) && 
+								Boolean(errors.lastName)}
+								helperText={touched.lastName && 
+								errors.lastName}
+								sx={{ gridColumn: 'span 2' }}
+								/>
+
+								<TextField 
+								label='Location'
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.location}
+								name='location'
+								error={Boolean(touched.location) && 
+								Boolean(errors.location)}
+								helperText={touched.location && 
+								errors.location}
+								sx={{ gridColumn: 'span 4' }}
+								/>
+
+								<TextField 
+								label='Occupation'
+								onBlur={handleBlur}
+								onChange={handleChange}
+								value={values.occupation}
+								name='occupation'
+								error={Boolean(touched.occupation) && 
+								Boolean(errors.occupation)}
+								helperText={touched.occupation && 
+								errors.occupation}
+								sx={{ gridColumn: 'span 4' }}
 								/>
 							</>
 						)}
