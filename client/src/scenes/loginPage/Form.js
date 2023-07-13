097de213c6c481fa96492f0c70805e3a0f9c4288
David Okaliwe <okaliwedavid@gmail.com>
2023-07-13 11:59:06 +0100

    Update

--- client/src/scenes/loginPage/Form.js
+++ client/src/scenes/loginPage/Form.js
@@ -17,11 +17,16 @@ import Dropzone from 'react-dropzone';
 import FlexBetween from 'components/FlexBetween';
 
 const resgisterSchema = yup.object().shape({
-	firstName:
-	lastName:
-	email:
-	password:
-	location:
-	occupation:
-	picture:
-})
+	firstName: yup.string().required('required'),
+	lastName: yup.string().required('required'),
+	email: yup.string().email('invalid email').required('required'),
+	password: yup.string().required('required'),
+	location: yup.string().required('required'),
+	occupation: yup.string().required('required'),
+	picture: yup.string().required('required')
+});
+
+const loginSchema = yup.object().shape({
+	email: yup.string().email('invalid email').required('required'),
+	password: yup.string().required('required')
+});
