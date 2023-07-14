daa5782b3c06b51ff64c3b056070d7147e82dfb1
David Okaliwe <okaliwedavid@gmail.com>
2023-07-14 19:46:32 +0100

    Update

--- client/src/App.js
+++ client/src/App.js
@@ -12,6 +12,7 @@ const App = () => {
 
   const mode = useSelector((state) => state.mode);
   const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  const isAuth = Boolean(useSelector((state) => state.token));
 
   return (
     <div className='app'>
@@ -20,8 +21,12 @@ const App = () => {
           <CssBaseline />
           <Routes>
             <Route path='/' element = {<LoginPage />} />
-            <Route path='/home' element = {<HomePage />} />
-            <Route path='/profile/:userId' element = {<ProfilePage />} />
+            <Route path='/home' 
+            element = {isAuth ? <HomePage /> : <Navigate to='/' />} 
+            />
+            <Route path='/profile/:userId' 
+            element = {isAuth ? <ProfilePage />: <Navigate to='/' />} 
+            />
           </Routes>
         </ThemeProvider>
       </BrowserRouter>
