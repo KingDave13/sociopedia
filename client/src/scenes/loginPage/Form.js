be403ae1b96e5bcb9ce0b0e0a85ab53976cc80e4
David Okaliwe <okaliwedavid@gmail.com>
2023-07-14 19:31:30 +0100

    Update

--- client/src/scenes/loginPage/Form.js
+++ client/src/scenes/loginPage/Form.js
@@ -80,6 +80,29 @@ const Form = () => {
 		}
 	};
 
+	const login = async(values, onSubmitProps) => {
+		const loggedInResponse = await fetch(
+			"http://localhost.3001/auth/login",
+			{
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json'},
+				body: JSON.stringify(values),
+			}
+		);
+		const loggedIn = await loggedInResponse.json();
+		onSubmitProps.resetForm();
+
+		if (loggedIn) {
+			dispatch(
+				setLogin({
+					user: loggedIn.user,
+					token: loggedIn.token
+				})
+			);
+			navigate('/home');
+		}
+	};
+
 
 	const handleFormSubmit = async (values, onSubmitProps) => {
 		if (isLogin) await login(values, onSubmitProps);
