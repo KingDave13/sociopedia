3434eb4dc215f355ec86dffc2f1e4fa430df92f8
David Okaliwe <okaliwedavid@gmail.com>
2023-07-15 09:58:56 +0100

    Update

---
+++ client/src/scenes/widgets/UserWidget.js
@@ -0,0 +1,9 @@
+import {
+	ManageAccountsOutlined,
+	EditOutlined,
+	LocationOnOutlined,
+	WorkOutlineOutlined
+} from '@mui/icons-material';
+import { Box, Typography, Divider, useTheme } from '@mui/material';
+import UserImage from 'components/UserImage';
+import FlexBetween from 'components/FlexBetween';
