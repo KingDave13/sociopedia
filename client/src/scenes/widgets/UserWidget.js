3ee2aa0590d74ab9fff728237db7375852e7ea18
David Okaliwe <okaliwedavid@gmail.com>
2023-07-15 10:24:50 +0100

    Update

--- client/src/scenes/widgets/UserWidget.js
+++ client/src/scenes/widgets/UserWidget.js
@@ -7,3 +7,58 @@ import {
 import { Box, Typography, Divider, useTheme } from '@mui/material';
 import UserImage from 'components/UserImage';
 import FlexBetween from 'components/FlexBetween';
+import WidgetWrapper from 'components/WidgetWrapper';
+import { useSelector } from 'react-redux';
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const UserWidget = ({ userId, picturePath }) => {
+	const [user, setUser] = useState(null);
+	const { palette } = useTheme();
+	const navigate = useNavigate();
+	const token = useSelector((state) => state.token);
+	const dark = palette.neutral.dark;
+	const medium = palette.neutral.medium;
+	const main = palette.neutral.main;
+
+	const getUser = async () => {
+		const response = await fetch(`http://localhost:3001/users/${userId}`,
+			{
+				method: 'GET',
+				headers: { Authorization: `Bearer ${token}` }
+			});
+		const data = await response.json();
+		setUser(data);
+	};
+
+	useEffect(() => {
+		getUser();
+	}, [])
+
+	if (!user) {
+		return null;
+	}
+
+	const {
+		firstName,
+		lastName,
+		location,
+		occupation,
+		viewedProfile,
+		impressions,
+		friends
+	} = user;
+
+	return (
+		<WidgetWrapper>
+			{/*FIRST ROW*/}
+			<FlexBetween
+			gap='0.5rem'
+			pb='1.1rem'
+			onClick={() => navigate(`/profile/${userId}`)}
+			>
+				
+			</FlexBetween>
+		</WidgetWrapper>
+	)
+};
