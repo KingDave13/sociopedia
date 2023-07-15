bb444e869f00140800c5eeb29b4afd19cc5c0c76
David Okaliwe <okaliwedavid@gmail.com>
2023-07-15 21:55:07 +0100

    Update

--- client/src/scenes/widgets/UserWidget.js
+++ client/src/scenes/widgets/UserWidget.js
@@ -134,7 +134,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
 				<FlexBetween gap='1rem' mb='0.5rem'>
 					<FlexBetween gap='1rem'>
-						<img src= '..assets/twitter.png' alt='twitter' />
+						<img src= '../assets/twitter.png' alt='twitter' />
 						<Box>
 							<Typography color={main} fontWeight='500'>
 								Twitter
@@ -149,7 +149,7 @@ const UserWidget = ({ userId, picturePath }) => {
 
 				<FlexBetween gap='1rem'>
 					<FlexBetween gap='1rem'>
-						<img src= '..assets/linkedin.png' alt='linkedin' />
+						<img src= '../assets/linkedin.png' alt='linkedin' />
 						<Box>
 							<Typography color={main} fontWeight='500'>
 								LinkedIn
