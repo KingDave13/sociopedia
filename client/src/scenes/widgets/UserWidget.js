d1e1de5eea615f310d2523310d0158f0faf35f79
David Okaliwe <okaliwedavid@gmail.com>
2023-07-15 13:24:49 +0100

    Update

--- client/src/scenes/widgets/UserWidget.js
+++ client/src/scenes/widgets/UserWidget.js
@@ -51,13 +51,13 @@ const UserWidget = ({ userId, picturePath }) => {
 
 	return (
 		<WidgetWrapper>
-
-			{/*FIRST ROW*/}
 			<FlexBetween
 			gap='0.5rem'
 			pb='1.1rem'
 			onClick={() => navigate(`/profile/${userId}`)}
 			>
+
+			{/*FIRST ROW*/}
 				<FlexBetween gap='1rem'>
 					<UserImage image={picturePath} />
 					<Box>
@@ -98,10 +98,69 @@ const UserWidget = ({ userId, picturePath }) => {
 						{occupation}
 					</Typography>
 				</Box>
+			</Box>
 
+			{/*THIRD ROW*/}
+			<Box p='1rem 0'>
+				<FlexBetween mb='0.5rem'>
+					<Typography color={medium}>
+						Who's viewed your profile
+					</Typography>
+					<Typography color={main} fontWeight='500'>
+						{viewedProfile}
+					</Typography>
+				</FlexBetween>
+
+				<FlexBetween>
+					<Typography color={medium}>
+						Impressions of your post
+					</Typography>
+					<Typography color={main} fontWeight='500'>
+						{impressions}
+					</Typography>
+				</FlexBetween>
+			</Box>
+
+			{/*FOURTH ROW*/}
+			<Box p='1rem 0'>
+				<Typography fontSize='1rem' color={main} fontWeight='500' mb='1rem'>
+					Social Profiles
+				</Typography>
+
+				<FlexBetween gap='1rem' mb='0.5rem'>
+					<FlexBetween gap='1rem'>
+						<img src= '..assets/twitter.png' alt='twitter' />
+						<Box>
+							<Typography color={main} fontWeight='500'>
+								Twitter
+							</Typography>
+							<Typography color={medium}>
+								Social Network
+							</Typography>
+						</Box>
+					</FlexBetween>
+					<EditOutlined sx={{ color: main }} />
+				</FlexBetween>
+
+				<FlexBetween gap='1rem'>
+					<FlexBetween gap='1rem'>
+						<img src= '..assets/linkedin.png' alt='linkedin' />
+						<Box>
+							<Typography color={main} fontWeight='500'>
+								LinkedIn
+							</Typography>
+							<Typography color={medium}>
+								Network Platform
+							</Typography>
+						</Box>
+					</FlexBetween>
+					<EditOutlined sx={{ color: main }} />
+				</FlexBetween>
 			</Box>
 
 			</FlexBetween>
 		</WidgetWrapper>
-	)
+	);
 };
+
+export default UserWidget;
