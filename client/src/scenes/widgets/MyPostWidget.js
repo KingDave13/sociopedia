bbbb6423e6dbbbac11d63618f9cff963b50cc437
David Okaliwe <okaliwedavid@gmail.com>
2023-07-16 13:55:26 +0100

    Update

--- client/src/scenes/widgets/MyPostWidget.js
+++ client/src/scenes/widgets/MyPostWidget.js
@@ -132,9 +132,36 @@ const MyPostWidget = ({ picturePath }) => {
 					color={mediumMain}
 					sx={{ '&:hover': { cursor: 'pointer', color: medium }}}
 					>
-						
+						Image
 					</Typography>
 				</FlexBetween>
+
+				{isNonMobileScreens ? (
+					<>
+						<FlexBetween gap='0.25rem'>
+							<GifBoxOutlined  sx={{ color: mediumMain }} />
+							<Typography color={mediumMain}>
+								Clip
+							</Typography>
+						</FlexBetween>
+
+						<FlexBetween gap='0.25rem'>
+							<AttachFileOutlined  sx={{ color: mediumMain }} />
+							<Typography color={mediumMain}>
+								Attachment
+							</Typography>
+						</FlexBetween>
+
+						<FlexBetween gap='0.25rem'>
+							<MicOutlined  sx={{ color: mediumMain }} />
+							<Typography color={mediumMain}>
+								Audio
+							</Typography>
+						</FlexBetween>
+					</>
+				) : (<FlexBetween gap='0.25rem'>
+						<MoreHorizOutlined sx={{ color: mediumMain }} />
+					</FlexBetween>)}
 			</FlexBetween>
 		</WidgetWrapper>
 	)
