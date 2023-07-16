5e881f2fe8fd8c1d13a7e6045331ff86bffd578e
David Okaliwe <okaliwedavid@gmail.com>
2023-07-16 15:55:07 +0100

    Update

--- client/src/scenes/widgets/PostWidget.js
+++ client/src/scenes/widgets/PostWidget.js
@@ -13,3 +13,23 @@ import {
 } from '@mui/material';
 import FlexBetween from 'components/FlexBetween';
 import Friend from 'components/Friend';
+import WidgetWrapper from 'components/WidgetWrapper';
+import { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { setPost } from 'state';
+
+const PostWidget = ({
+	postId,
+	postUserId,
+	name,
+	description,
+	location,
+	picturePath,
+	userPicturePath,
+	likes,
+	comments
+}) => {
+	
+}
+
+export default PostWidget;
