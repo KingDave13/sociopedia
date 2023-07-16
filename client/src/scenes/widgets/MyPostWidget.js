e2204814d16b61a3ccb88317f970a463b9a4ef29
David Okaliwe <okaliwedavid@gmail.com>
2023-07-16 13:32:53 +0100

    Update

--- client/src/scenes/widgets/MyPostWidget.js
+++ client/src/scenes/widgets/MyPostWidget.js
@@ -121,6 +121,21 @@ const MyPostWidget = ({ picturePath }) => {
 					</Dropzone>
 				</Box>
 			)}
+
+			<Divider sx={{ margin: '1.25rem 0'}} />
+
+			<FlexBetween>
+				<FlexBetween gap='0.25rem' onClick={() => 
+				setIsImage(!isImage)}>
+					<ImageOutlined sx={{ color: mediumMain }} />
+					<Typography
+					color={mediumMain}
+					sx={{ '&:hover': { cursor: 'pointer', color: medium }}}
+					>
+						
+					</Typography>
+				</FlexBetween>
+			</FlexBetween>
 		</WidgetWrapper>
 	)
 };
