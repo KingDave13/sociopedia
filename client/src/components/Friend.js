141abffcc285360d1fcc992e367f68c45f9a363d
David Okaliwe <okaliwedavid@gmail.com>
2023-07-16 15:24:28 +0100

    Update

---
+++ client/src/components/Friend.js
@@ -0,0 +1,27 @@
+import {
+	PersonAddOutlined, PersonRemoveOutlined
+} from '@mui/icons-material';
+import { Box, IconButton, Typography, useTheme } from '@mui/material';
+import { useDispatch, useSelector } from 'react-redux';
+import { setFriends } from 'state';
+import FlexBetween from './FlexBetween';
+import UserImage from './UserImage';
+
+const Friend = ({ friendId, name, subtitle, userPicturePath }) => {
+	
+	const dispatch = useDispatch();
+	const navigate = useNavigate();
+	const { _id } = useSelector((state) => state.user);
+	const token = useSelector((state) => state.token);
+	const friends = useSelector((state) => state.user.friends);
+
+	const { palette } = useTheme();
+	const primaryLight = palette.primary.light;
+	const primaryDark = palette.primary.dark;
+	const main = palette.neutral.main;
+	const medium = palette.neutral.medium;
+
+	const isFriend = 
+}
+
+export default Friend;
