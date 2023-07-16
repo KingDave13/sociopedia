8f67535a856c99278e4fedbc3e8e90ae44911b07
David Okaliwe <okaliwedavid@gmail.com>
2023-07-16 21:18:00 +0100

    Update

--- client/src/scenes/widgets/PostWidget.js
+++ client/src/scenes/widgets/PostWidget.js
@@ -1,6 +1,6 @@
 import { 
 	ChatBubbleOutlineOutlined, 
-	FavoriteBorderOutlines,  
+	FavoriteBorderOutlined,  
 	FavoriteOutlined, 
 	ShareOutlined
 } from '@mui/icons-material';
@@ -65,6 +65,59 @@ const PostWidget = ({
 			<Typography color={main} sx={{ mt: '1rem' }}>
 				{description}
 			</Typography>
+			{picturePath && (
+				<img 
+				width='100%'
+				height'='auto'
+				alt='post'
+				style={{ borderRadius: '0.75rem', marginTop: '0.75rem' }}
+				src={`http://localhost:3001/assets/${picturePath}`}
+				/>
+			)}
+			<FlexBetween mt='0.25rem'>
+				<FlexBetween gap='1rem'>
+
+					<FlexBetween gap='0.3rem'>
+						<IconButton onClick={patchLike}>
+							{isLiked ? (
+								<FavoriteOutlined sx={{ color:primary }} />
+							) : (
+								<FavoriteBorderOutlined />
+							)}
+						</IconButton>
+
+						<Typography>{likeCount}</Typography>
+					</FlexBetween>
+
+					<FlexBetween gap='0.3rem'>
+						<IconButton onClick={() => setIsComments(!isComments)}>
+							<ChatBubbleOutlineOutlined />
+						</IconButton>
+
+						<Typography>{comments.length}</Typography>
+					</FlexBetween>
+
+				</FlexBetween>
+
+				<IconButton>
+					<ShareOutlined />
+				</IconButton>
+			</FlexBetween>
+			{isComments && (
+				<Box mt='0.5rem'>
+					{comments.map((comment, i) => (
+						<Box key={`${name}-${i}`}>
+							<Divider />
+							<Typography
+							sx={{ color: main, m:'0.5rem 0', pl: '1rem' }}
+							>
+								{comment}
+							</Typography>
+						</Box>
+					))}
+					<Divider />
+				</Box>
+			)}
 		</WidgetWrapper>
 	)
 };
