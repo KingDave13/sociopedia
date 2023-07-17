9f9a3054079cb729aaf0b722d4aa35163d451866
David Okaliwe <okaliwedavid@gmail.com>
2023-07-17 17:20:20 +0100

    Update

--- client/src/scenes/widgets/AdvertWidget.js
+++ client/src/scenes/widgets/AdvertWidget.js
@@ -2,3 +2,8 @@ import { Typography, useTheme } from '@mui/material';
 import FlexBetween from 'components/FlexBetween';
 import WidgetWrapper from 'components/WidgetWrapper';
 
+const AdvertWidget = () = {
+
+};
+
+export default AdvertWidget;
