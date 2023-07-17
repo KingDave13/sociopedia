163cea7c4c36b0762978e5ae25402fb91f90d476
David Okaliwe <okaliwedavid@gmail.com>
2023-07-17 18:22:29 +0100

    Update

--- client/src/scenes/profilePage/index.js
+++ client/src/scenes/profilePage/index.js
@@ -1,4 +1,12 @@
-
+import { Box, useMediaQuery } from '@mui/material';
+import { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { useParams } from 'react-router-dom';
+import Navbar from 'scenes/navbar';
+import FriendListWidget from 'scenes/widgets/FriendListWidget';
+import MyPostWidget from 'scenes/widgets/MyPostWidget';
+import PostsWidget from 'scenes/widgets/PostsWidget';
+import UserWidget from 'scenes/widgets/UserWidget';
 
 const ProfilePage = () => {
 	return (
@@ -6,6 +14,6 @@ const ProfilePage = () => {
 			profilepage
 		</div>
 	)
-}
+};
 
 export default ProfilePage;
